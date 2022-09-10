d4049edc2ab7bdf22005326930970762ca6ee9ec
hikunsa05 <113221885+hikunsa05@users.noreply.github.com>
2022-09-10 09:41:29 +0700

    file pelatihan coding
    pelatihan pertama

---
+++ main.py
@@ -0,0 +1,26 @@
+nama = "Nama saya Fira"
+angka = "12"
+angka_pecahan = 1.5
+tipe_boolean = False
+tipe_list = ["Fira", "Ilmi", "Raissa", 11]
+tipe_set = {1,2,3,3,3,4,4, "abc"}
+pertambahan = 2004 + 1997
+pengurangan = 2022 - 1997
+perkalian = "ha" * 3
+pembagian = 12 / 4
+pembagian_habis_bagi = 20 // 6
+modulus = 17 % 2
+
+print(nama)
+print("tipe float", angka)
+print("boolean : ")
+print(tipe_boolean)
+print(tipe_list)
+print(tipe_list[2])
+print(tipe_set)
+print(pertambahan)
+print(pengurangan)
+print(perkalian)
+print(pembagian)
+print(pembagian_habis_bagi)
+print(modulus)
